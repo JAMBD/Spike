98b19b0550ce00bdde0ce41c036d92e5356ffb00
John Lam <john.lam128@gmail.com>
2015-05-12 21:38:06 +1000

    init commit

---
+++ src/com/rover/spike/TaskDistributer.java
@@ -0,0 +1,143 @@
+package com.rover.spike;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.LinkedList;
+import java.util.Set;
+
+import messages.Complete;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class TaskDistributer {
+
+	private static void printUsage(){
+		System.out.print("-b <broker address, eg: tcp://192.168.0.100:1883>\n");
+		System.out.print("-c <client id string name>\n");
+		System.out.print("-f <list of folder containing batches of tasks, must be last arg>\n");
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		LinkedList<String> taskRepoFldr = new LinkedList<String>();
+		int qos             = 2;
+		String broker       = "tcp://192.168.0.100:1883";
+		String clientId     = "taskDist";
+		int batches			= 1;
+        MemoryPersistence persistence = new MemoryPersistence();
+		//input args
+		for(int i=0;i<args.length;i++){
+			if(args[i].compareTo("-b")==0){
+				if(++i < args.length){
+					broker = args[i++];
+				}else{
+					printUsage();
+					break;
+				}
+			}
+			if(args[i].compareTo("-c")==0){
+				if(++i < args.length){
+					clientId = args[i++];
+				}else{
+					printUsage();
+					break;
+				}
+			}
+
+			if(args[i].compareTo("-n")==0){
+				if(++i < args.length){
+					batches = Integer.parseInt(args[i++]);
+				}else{
+					printUsage();
+					break;
+				}
+			}
+			if(args[i].compareTo("-f")==0){
+				if(!(i+1 < args.length)){
+					printUsage();
+					break;
+				}
+				while(++i < args.length){
+					taskRepoFldr.add(args[i++]);
+				}
+				break;
+			}
+		}
+		if(taskRepoFldr.isEmpty()){
+			System.err.print("No input folders\n");
+			System.exit(-1);
+		}
+			
+		try {
+
+            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            System.out.println("Connecting to broker: "+broker);
+            sampleClient.connect(connOpts);
+            System.out.println("Connected");
+    		for(int i=0;i<batches;i++){
+    			String current = taskRepoFldr.pop();
+    			TaskRepository taskBatch = new TaskRepository(current);
+    			Set<String> tasksIds = taskBatch.taskDefinitions.keySet();
+                
+                for(String task : tasksIds){
+                	ByteArrayOutputStream b = new ByteArrayOutputStream();
+                	try {
+                		ObjectOutputStream o = new ObjectOutputStream(b);
+                		o.writeObject(taskBatch.GetTask(task));
+                		o.close();
+                	} catch (IOException e) {
+                		e.printStackTrace();
+                	}
+
+                	byte bytes[]=b.toByteArray();
+
+                	System.out.println("Publishing task: "+ task);
+                	MqttMessage message = new MqttMessage(bytes);
+                	message.setQos(qos);
+                	sampleClient.publish("tasklist", message);
+                	System.out.println("Message published");
+                }
+                
+
+    			sampleClient.setCallback(taskBatch);
+                sampleClient.subscribe("complete");
+                
+                while(!taskBatch.complete()){
+                	try {
+                		Thread.sleep(1000);
+                	} catch (InterruptedException e) {
+                		e.printStackTrace();
+                	}
+                }
+                
+                sampleClient.unsubscribe("complete");
+                sampleClient.setCallback(null);
+    			taskRepoFldr.addLast(current);
+    		}
+            
+            sampleClient.disconnect();
+            System.out.println("Disconnected");
+            System.exit(0);
+        } catch(MqttException me) {
+            System.out.println("reason "+me.getReasonCode());
+            System.out.println("msg "+me.getMessage());
+            System.out.println("loc "+me.getLocalizedMessage());
+            System.out.println("cause "+me.getCause());
+            System.out.println("excep "+me);
+            me.printStackTrace();
+        }
+		
+
+	}
+
+}
---
+++ src/com/rover/spike/TaskRepository.java
@@ -0,0 +1,167 @@
+package com.rover.spike;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import messages.Complete;
+import messages.Method;
+import messages.Task;
+import messages.Task.AgentTypes;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class TaskRepository implements MqttCallback {
+
+	boolean debugFlag = true;
+	String repositoryFolderPath = "";
+	Map<String,Task> taskDefinitions;
+
+	public void markDone(String taskId){
+		System.out.print("Task: " + taskId + " complete.\n");
+		taskDefinitions.remove(taskId);
+	}
+
+	public boolean complete(){
+		return taskDefinitions.isEmpty();
+	}
+
+	public TaskRepository(String repositoryFolderPath)
+	{
+		this.repositoryFolderPath = repositoryFolderPath;
+		this.taskDefinitions= new HashMap<String,Task>();
+		File folder = new File(repositoryFolderPath);
+		for(File i : folder.listFiles()){
+			ReadTaskDescriptions(i);
+		}
+	}
+
+	public void ReadTaskDescriptions(File file)
+	{
+		try {
+			FileInputStream fis = null;
+			fis = new FileInputStream(file);
+			//Get the DOM Builder Factory
+			DocumentBuilderFactory factory =  DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document document = builder.parse(fis);
+			NodeList nodeList = document.getDocumentElement().getChildNodes();
+			for (int i = 0; i < nodeList.getLength(); i++) {
+				//We have encountered a <Taems> tag.
+				Node node = nodeList.item(i);
+				if (node instanceof Element) {
+					Task t = ParseTask(node);
+					this.taskDefinitions.put(t.label, t);
+					System.out.print("Task " + t.label+ " added to repository\n");
+				}
+			}
+		} 
+		catch (IOException e) {}
+		catch(ParserConfigurationException e){}
+		catch(SAXException e){}
+	}
+
+	public Task GetTask(String name)
+	{
+		if (!this.taskDefinitions.containsKey(name))
+		{
+			System.err.print("Possible Error: Task " + name + " not found in repository\n");
+		}
+		return this.taskDefinitions.get(name);
+	}
+
+	static int uid = 0;
+	private Task ParseTask(Node node)
+	{
+		String taskId;
+		String taskName;
+		String qafStringValue;
+		boolean recurring = false;
+		boolean isTask = node.getNodeName()=="Task";
+		taskId = node.getAttributes().getNamedItem("id").getNodeValue() + uid++;
+		taskName = node.getAttributes().getNamedItem("name").getNodeValue();
+		if (isTask) {
+			Node recurringNode = node.getAttributes().getNamedItem("recurring");
+			if (recurringNode!=null)
+				recurring = recurringNode.getNodeValue().toString().toLowerCase().equalsIgnoreCase("true");
+			qafStringValue = node.getAttributes().getNamedItem("qaf").getNodeValue().toString().toLowerCase();
+		}
+		AgentTypes type = AgentTypes.AMBULANCE;
+		if (taskName.equalsIgnoreCase("Police"))
+		{
+			type = AgentTypes.POLICE;
+		}
+		else if (taskName.equalsIgnoreCase("Ambulance"))
+		{
+			type = AgentTypes.AMBULANCE;
+		}
+		Task task = new Task(taskId, type);
+		NodeList children = node.getChildNodes();
+		for (int i = 0; i < children.getLength(); i++) {
+			Node childNode = children.item(i);
+			if (childNode.getNodeName().equalsIgnoreCase("Method"))
+			{
+				messages.Method childMethod = ParseMethod(childNode);
+				task.addNode(childMethod);
+			}
+			else if (childNode.getNodeName().equalsIgnoreCase("Task"))
+			{
+				Task childTask = ParseTask(childNode);
+				task.addNode(childTask);
+			}
+		}
+		return task;
+	}
+
+	private messages.Method ParseMethod(Node node)
+	{
+		String methodName = node.getAttributes().getNamedItem("name").getNodeValue();
+		int quality = Integer.parseInt(node.getAttributes().getNamedItem("Quality").getNodeValue());
+		int duration = Integer.parseInt(node.getAttributes().getNamedItem("Duration").getNodeValue());
+		int xCoord = Integer.parseInt(node.getAttributes().getNamedItem("XCoord").getNodeValue());
+		int yCoord = Integer.parseInt(node.getAttributes().getNamedItem("YCoord").getNodeValue());
+		Method method = new Method(methodName,(double)xCoord,(double)yCoord,System.nanoTime(),quality,duration);
+		return method;
+	}
+
+	@Override
+	public void connectionLost(Throwable arg0) {
+	}
+
+	@Override
+	public void deliveryComplete(IMqttDeliveryToken arg0) {
+	}
+
+	@Override
+	public void messageArrived(String arg0, MqttMessage mesg) throws Exception {		
+		ByteArrayInputStream b1 = new ByteArrayInputStream(mesg.getPayload());
+		ObjectInputStream o1 = new ObjectInputStream(b1);
+		Object unknownMsg = o1.readObject();
+
+		if (unknownMsg.getClass() == Complete.class) {
+			Complete done = (Complete) unknownMsg;
+			markDone(done.taskId);
+		} else {
+			System.out.print("Spike: Unknown mqtt message\n");
+		}
+	}
+}
